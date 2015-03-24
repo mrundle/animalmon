89ad35cd50433e681747787418ea4311e88b1a8b
amarti36 <amarti36@nd.edu>
2015-03-24 15:43:07 -0400

    modifying status 19

--- api/battleMechanics/battleCalculations.php
+++ api/battleMechanics/battleCalculations.php
@@ -11,24 +11,26 @@ function moveTypeCalculation($team){
 	$selfAttributes = $_SESSION[$team][$_SESSION[$team]['currentAnimalmon']]['STATS'];
 	$foeAttributes = $_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS'];
 	if($moveAttributes['TARGET'] == 'Foe'){
-		if(hitCalculation($team, $moveAttributes['BASE_ACCURACY'], $selfAttributes['ACCURACY'], $foeAttributes['EVASION'])){
-			if($moveAttributes['BASE_DAMAGE'] != 0){
-				$_SESSION[$team]['battleLog']['move_type'] = 'damage';
-				$damage = damageCalculation($team, $moveAttributes['BASE_DAMAGE'], $moveAttributes['CRITICAL_HIT'], $selfAttributes['ATTACK'], $foeAttributes['DEFENSE'], $selfAttributes['SPEED'], $foeAttributes['SPEED']);
-				$_SESSION[$team]['battleLog']['damage'] = $damage;
-				$_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS']['HEALTH'] -= $damage;
-				if($_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS']['HEALTH'] < 1) {
-					$_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS']['HEALTH'] = 0;
-				}
-			}
-			else{
-				$_SESSION[$team]['battleLog']['move_type'] = 'status';
-				$_SESSION[$team]['battleLog']['target'] = $moveAttributes['TARGET'];
-				$_SESSION[$team]['battleLog']['effect'] = $moveAttributes['EFFECT'];
-				$_SESSION[$team]['battleLog']['target2'] = $moveAttributes['TARGET2'];
-				$_SESSION[$team]['battleLog']['effect2'] = $moveAttributes['EFFECT2'];
-			}
+		hitCalculation($team, $moveAttributes['BASE_ACCURACY'], $selfAttributes['ACCURACY'], $foeAttributes['EVASION'])
+	}
+	else{
+		hitCalculation($team, 100, 1, 1);
+	}
+	if($moveAttributes['BASE_DAMAGE'] != 0){
+		$_SESSION[$team]['battleLog']['move_type'] = 'damage';
+		$damage = damageCalculation($team, $moveAttributes['BASE_DAMAGE'], $moveAttributes['CRITICAL_HIT'], $selfAttributes['ATTACK'], $foeAttributes['DEFENSE'], $selfAttributes['SPEED'], $foeAttributes['SPEED']);
+		$_SESSION[$team]['battleLog']['damage'] = $damage;
+		$_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS']['HEALTH'] -= $damage;
+		if($_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS']['HEALTH'] < 1) {
+			$_SESSION[$foeTeam][$_SESSION[$foeTeam]['currentAnimalmon']]['STATS']['HEALTH'] = 0;
 		}
+	}
+	else{
+		$_SESSION[$team]['battleLog']['move_type'] = 'status';
+		$_SESSION[$team]['battleLog']['target'] = $moveAttributes['TARGET'];
+		$_SESSION[$team]['battleLog']['effect'] = $moveAttributes['EFFECT'];
+		$_SESSION[$team]['battleLog']['target2'] = $moveAttributes['TARGET2'];
+		$_SESSION[$team]['battleLog']['effect2'] = $moveAttributes['EFFECT2'];
 	}
 }
 function accuracyCalculation($baseAccuracy, $statAccuracy, $statEvasion){
