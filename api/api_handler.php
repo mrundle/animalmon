fd872384f7b86e792d28770e27357ba33f35ce58
Andres Martin <you@example.com>
2015-03-30 15:46:50 -0400

    changing blog

--- api/api_handler.php
+++ api/api_handler.php
@@ -143,6 +143,7 @@
 
 	function handleSwap($animalmon){
 		$_SESSION['battleTeam1']['currentAnimalmon'] = $animalmon;
+		$_SESSION['battleLog'] = $animalmon . " was swapped in!<br>";
 		return $_SESSION;
 	}
 
