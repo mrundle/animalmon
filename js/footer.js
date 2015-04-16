3a305ac2338ee7d8eeee415ef486dadc6112dfee
Matthew Norris Rundle <mrundle@csevm03.virtual.crc.nd.edu>
2015-04-16 11:20:26 -0400

    added footer and header js

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+// TO INCLUDE THIS HEADER, SIMPLY USE THIS IN THE CODE:
+// <script src="csevm03.crc.nd.edu:8503/project/js/footer.js"></script>
+document.write('\
+<hr style="margin-top:120px; margin-bottom:30px;">\
+<footer style="font-size:small">\
+    <center>\
+        <p>Created by Andres Martin, Matt Mahan, and Matt Rundle. View our <a href="http://csevm03.crc.nd.edu:8503/project/info.html">project page</a> to see this project\'s design documents.</p>\
+    </center>\
+</footer>\
+');
