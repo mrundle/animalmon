b2dde4a9332e5e801c50f51de45da8c3cbee18a8
Matthew Norris Rundle <mrundle@csevm03.virtual.crc.nd.edu>
2015-04-16 10:55:55 -0400

    header coming from js file now

---
+++ js/header.js
@@ -0,0 +1,9 @@
+// TO INCLUDE THIS HEADER, SIMPLY USE THIS IN THE CODE:
+// <script src="csevm03.crc.nd.edu:8503/project/js/header.js"></script>
+document.write('\
+<center>\
+    <img src="http://img1.wikia.nocookie.net/__cb20100714172711/u__/unanswers/images/thumb/8/88/Real_animals_made_to_look_like_pokemon.png/500px-Real_animals_made_to_look_like_pokemon.png" alt="Insert cool picture here.">\
+    <h1>Animalmon!</h1>\
+    <hr style="margin-top:30px; margin-bottom:30px;">\
+</center>\
+');
